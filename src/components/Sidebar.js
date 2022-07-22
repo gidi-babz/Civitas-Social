f77d5999d0fc19fee3ab0baefd90059025e82aa8
Gidi Babz <tunex4ind@gmail.com>
2022-07-22 16:05:33 +0100

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,91 @@
-import { Box } from '@mui/material';
+import {
+	Box,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
+	Switch,
+} from '@mui/material';
+
+import HomeIcon from '@mui/icons-material/Home';
+import ArticleIcon from '@mui/icons-material/Article';
+import GroupIcon from '@mui/icons-material/Group';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 const Sidebar = () => {
 	return (
 		<Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
-			Sidebar
+			<List>
+				<ListItem disablePadding>
+					<ListItemButton component="a" href="#home">
+						<ListItemIcon>
+							<HomeIcon />
+						</ListItemIcon>
+						<ListItemText primary="Homepage" />
+					</ListItemButton>
+				</ListItem>
+				<ListItem disablePadding>
+					<ListItemButton component="a" href="#home">
+						<ListItemIcon>
+							<ArticleIcon />
+						</ListItemIcon>
+						<ListItemText primary="Pages" />
+					</ListItemButton>
+				</ListItem>
+				<ListItem disablePadding>
+					<ListItemButton component="a" href="#home">
+						<ListItemIcon>
+							<GroupIcon />
+						</ListItemIcon>
+						<ListItemText primary="Groups" />
+					</ListItemButton>
+				</ListItem>
+				<ListItem disablePadding>
+					<ListItemButton component="a" href="#home">
+						<ListItemIcon>
+							<StorefrontIcon />
+						</ListItemIcon>
+						<ListItemText primary="Marketplace" />
+					</ListItemButton>
+				</ListItem>
+				<ListItem disablePadding>
+					<ListItemButton component="a" href="#home">
+						<ListItemIcon>
+							<PersonIcon />
+						</ListItemIcon>
+						<ListItemText primary="Friends" />
+					</ListItemButton>
+				</ListItem>
+				<ListItem disablePadding>
+					<ListItemButton component="a" href="#home">
+						<ListItemIcon>
+							<SettingsIcon />
+						</ListItemIcon>
+						<ListItemText primary="Settnngs" />
+					</ListItemButton>
+				</ListItem>
+				<ListItem disablePadding>
+					<ListItemButton component="a" href="#home">
+						<ListItemIcon>
+							<AccountBoxIcon />
+						</ListItemIcon>
+						<ListItemText primary="Profile" />
+					</ListItemButton>
+				</ListItem>
+				<ListItem disablePadding>
+					<ListItemButton component="a" href="#home">
+						<ListItemIcon>
+							<DarkModeIcon />
+						</ListItemIcon>
+						<Switch />
+					</ListItemButton>
+				</ListItem>
+			</List>
 		</Box>
 	);
 };
